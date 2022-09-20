fd491832daf727c1aa918bdfc52cf8a43aa19478
prince-inapp <109217393+prince-inapp@users.noreply.github.com>
2022-09-20 17:39:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var headers = new Headers();
+headers.append("X-CSCAPI-KEY", "QnFSdDFqUVpPY2NOdWNJZmZTMVRJTG1LWWV3M0JJUDR1cWFZMktreA==");
+
+var requestOptions = {
+   method: 'GET',
+   headers: headers,
+   redirect: 'follow',
+};
+
+// api function which returns the data
+async function loadData() {
+    const response = await fetch('https://api.countrystatecity.in/v1/countries/', requestOptions);
+    const data = await response.json();
+    console.log(typeof data);
+    loadOptions(data, 'select-country')
+
+}
+
+async function loadStates(){
+
+    const country = document.getElementById('select-country').value;
+    console.log(country)
+    
+    const stateResponse = await fetch('https://api.countrystatecity.in/v1/countries/' + country + '/states', requestOptions);
+    const stateData = await stateResponse.json();
+    console.log(stateData);
+    loadOptions(stateData, 'select-state');
+    loadCities();
+}
+
+async function loadCities(){
+    const country = document.getElementById('select-country').value;
+    const state = document.getElementById('select-state').value;
+    document.getElementById("select-state").options.length=0;
+    const cityResponse = await fetch(
+        'https://api.countrystatecity.in/v1/countries/'+country+'/states/'+state+'/cities', requestOptions);
+    const cityData = await cityResponse.json();
+    console.log(cityData)
+    loadOptions(cityData, 'select-city')
+    
+}
+
+function loadOptions(data, id){
+
+    const options = data.map((item) => {
+        let selectCountry = document.getElementById(id);
+        var opt = document.createElement('option');
+        opt.value = item.iso2;
+        opt.innerText = item.name;
+        selectCountry.appendChild(opt);
+    });
+}
+
+
